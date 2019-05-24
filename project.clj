63edbe2cd4e4e7ccb215c7d7b248b9ffe0234ea2
Dainius Jocas <dainius.jocas@tokenmill.lt>
2019-05-24 16:49:08 +0300

    refactor: proper license note in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clojure-graalvm-aws-lambda/lein-template "0.3.0"
   :description "Template for AWS Lambda with Clojure tools-deps, GraalVM, Docker, and Gitlab CI"
   :url "https://gitlab.com/Jocas/clojure-graalvm-aws-lambda-template"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "Apache License Version 2.0, January 2004"
+            :url "http://www.apache.org/licenses/"}
   :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org"}]
                         ["snapshots" {:sign-releases false :url "https://clojars.org"}]]
   :eval-in-leiningen true)
