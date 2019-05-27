4d50d1c5f038586bcd551e95552e5c4108bc7d9e
Dainius Jocas <dainius.jocas@tokenmill.lt>
2019-05-27 08:43:59 +0300

    refactor: typo

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojure-graalvm-aws-lambda/lein-template "0.3.1"
   :description "Template for AWS Lambda with Clojure tools-deps, GraalVM, Docker, and Gitlab CI"
-  :url "hhttps://github.com/tokenmill/clojure-graalvm-aws-lambda-template"
+  :url "https://github.com/tokenmill/clojure-graalvm-aws-lambda-template"
   :license {:name "Apache License Version 2.0, January 2004"
             :url "http://www.apache.org/licenses/"}
   :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org"}]
