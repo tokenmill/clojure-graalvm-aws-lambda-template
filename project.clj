45d15854f5fb2ebf16c4cc4ec17acd71ec3de57a
Dainius Jocas <dainius.jocas@tokenmill.lt>
2019-05-27 08:57:06 +0300

    7 scm link (#8)
    * chore: SCM link
    * chore: version bump, changelog, /close #7

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject clojure-graalvm-aws-lambda/lein-template "0.3.1"
+(defproject clojure-graalvm-aws-lambda/lein-template "0.3.2"
   :description "Template for AWS Lambda with Clojure tools-deps, GraalVM, Docker, and Gitlab CI"
   :url "https://github.com/tokenmill/clojure-graalvm-aws-lambda-template"
   :license {:name "Apache License Version 2.0, January 2004"
             :url "http://www.apache.org/licenses/"}
   :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org"}]
                         ["snapshots" {:sign-releases false :url "https://clojars.org"}]]
+  :scm {:name "git"
+        :url "https://github.com/tokenmill/clojure-graalvm-aws-lambda-template"}
   :eval-in-leiningen true)
