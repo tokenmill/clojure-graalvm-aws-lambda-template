495f9b5d40ec828e4a74a6caf1e744fee19bfe9d
Dainius Jocas <dainius.jocas@tokenmill.lt>
2019-05-27 08:55:31 +0300

    chore: version bump, changelog, /close #7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-graalvm-aws-lambda/lein-template "0.3.1"
+(defproject clojure-graalvm-aws-lambda/lein-template "0.3.2"
   :description "Template for AWS Lambda with Clojure tools-deps, GraalVM, Docker, and Gitlab CI"
   :url "https://github.com/tokenmill/clojure-graalvm-aws-lambda-template"
   :license {:name "Apache License Version 2.0, January 2004"
